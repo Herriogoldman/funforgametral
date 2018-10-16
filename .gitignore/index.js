36d7268d032a2415e97825ddb515c2d716ee7559
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2018-10-16 21:52:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -244,4 +244,8 @@ bot.on('message', message => {
         .setImage(ah)
         message.channel.send(ah_embed)
     };
+    
+    if (message.content.startsWhith('aide')){
+        message.channel.send('Voilà les différentes commandes :' \n '- TA MERE !' \n '- Pour les fameuses photos, faire "sendimages" (AUTORISE SEULEMENT DANS LE SALON DEDIE !)')
+    };
 });
