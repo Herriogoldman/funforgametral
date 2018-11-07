58486a299f3bbc541db79deb13559a20ca33d384
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2018-11-07 17:50:01 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -405,7 +405,7 @@ bot.on('message', message => {
         
         if (message.content === '2'){
 
-            var bien = [
+            var bien2 = [
 
                 "http://img.over-blog-kiwi.com/0/62/06/01/20140701/ob_03acc5_les-plus-belles-femmes-nues-de-l-ete-4.jpg",
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZT0E0MTcJpcLYChSljSd3kagEzqcHgP0cvEzlvcf8olU3nWjStA",
@@ -582,26 +582,23 @@ bot.on('message', message => {
                 "https://www.pichunter.com/gallery/3648684/Adria_Rae_finally_gets_the#13&gid=1&pid=14"
             ];
 
-            var oui = bien[Math.floor(Math.random() * bien.length)];
+            var oui2 = bien2[Math.floor(Math.random() * bien2.length)];
 
             message.channel.send('Très bien, définissez un chiffre différent entre 1 et 2 pour chacun de vous. Faites la commande "Go image" pour jouer.')
             while (message.content !== '&Fin du game'){
                 if (message.content === 'Go image'){
 
-                    var bien_embed = new Discord.RichEmbed()
-                    .setImage(oui)
+                    var bien2_embed = new Discord.RichEmbed()
+                    .setImage(oui2)
                     .setTitle('Joueur 1')
-                    message.channel.send(bien_embed)
+                    message.channel.send(bien2_embed)
 
-                    var bien_embed = new Discord.RichEmbed()
-                    .setImage(oui)
+                    var bien3_embed = new Discord.RichEmbed()
+                    .setImage(oui2)
                     .setTitle('Joueur 2')
-                    message.channel.send(bien_embed)
+                    message.channel.send(bien3_embed)
 
                     message.channel.send("Vous pouvez maintenant votez pour l'image que vous préferez en envoyant le chiffre du joueur (j1 ou j2). Vous pouvez de continuez à jouer autant de temps que vous le souhaitez. Dès que vous voulez que le jeu se finnisse faites '&Fin du game'")
-
-                    var j1 = 0;
-                    var j2 = 0;
 
                     if (message.content === 'j1'){
                         var j1 = j1 + 1;
