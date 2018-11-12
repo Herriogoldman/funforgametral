09e68fa146b7b3097195cacf2bda8a078b60caac
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2018-11-12 20:30:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -596,7 +596,8 @@ bot.on('message', message => {
 
         message.channel.send("Vous pouvez maintenant votez pour l'image que vous préferez en envoyant le chiffre du joueur (j1 ou j2). Vous pouvez continuez à jouer autant de temps que vous le souhaitez. Dès que vous voulez que le jeu se finnisse faites '&Fin du game'")
     };
-    
+    var j1 = 0;
+   
     if (message.content === '&Debut du game'){
         message.channel.send('Debut du jeu, combien de joueurs ? (Rajouter "&" devant le chiffre et sans espace)')
     };
