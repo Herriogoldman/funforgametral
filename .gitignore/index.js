0290ae65bdca594553c8a0f0a3a7534ebc1160fc
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2018-11-12 19:31:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 
 var bot = new Discord.Client();
-var j1 = 0;
 
 bot.on('ready', () => {
     console.log("Bot Ready !");
@@ -10,7 +9,6 @@ bot.on('ready', () => {
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {  
-    var j1 = 0;
     if (message.content.startsWith ("sendimages")){
         var bien = [
             
@@ -599,11 +597,12 @@ bot.on('message', message => {
         message.channel.send("Vous pouvez maintenant votez pour l'image que vous préferez en envoyant le chiffre du joueur (j1 ou j2). Vous pouvez continuez à jouer autant de temps que vous le souhaitez. Dès que vous voulez que le jeu se finnisse faites '&Fin du game'")
     };
     
+    var j1 = 0;
     if (message.content === '&Debut du game'){
         message.channel.send('Debut du jeu, combien de joueurs ? (Rajouter "&" devant le chiffre et sans espace)')
     };
     if (message.content === "j1"){
-        j1 = j1 + 1;
+        int j1 = j1 + 1;
     };
     if (message.content === '&Fin du game'){
         message.channel.send("Fin du jeu. Bien joué au vainqueur " + j1 + " (Là le bot est censé afficher qui est le vainqueur mais j'ai galéré 2h pour rien trouver au final dsl)")
