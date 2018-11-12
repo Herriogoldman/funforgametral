3432508458f00e34edd000bb86ca3afa46c70450
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2018-11-12 20:29:10 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -602,7 +602,7 @@ bot.on('message', message => {
     };
     if (message.content === "j1"){
         var j1 = j1 + 1;
-        message.channel.send('j1')
+        message.channel.send(j1)
     };
     if (message.content === '&Fin du game'){
         message.channel.send("Fin du jeu. Bien joué au vainqueur " + j1 + " (Là le bot est censé afficher qui est le vainqueur mais j'ai galéré 2h pour rien trouver au final dsl)")
