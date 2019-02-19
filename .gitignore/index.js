65334c026a10efdfd8309894b97e55ca8f9cc934
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2019-02-19 16:43:32 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
-const Joueur1 = require('funforgametral/.gitignore/j1')
-const Joueur2 = require('funforgametral/.gitignore/j2')
+
 var bot = new Discord.Client();
 
 bot.on('ready', () => {
@@ -437,9 +436,6 @@ bot.on('message', message => {
         message.channel.send("Fin du jeu. Bien joué au vainqueur " + j1 + " (Là le bot est censé afficher qui est le vainqueur mais j'ai galéré 2h pour rien trouver au final dsl)")
     };    
     
-    if (message.content === 'j1'){
-        message.channel.send(j1)
-    };
     
     if (message.content === '&3'){
     message.channel.send('Très bien, définissez un chiffre différent entre 1 et 2 pour chacun de vous. Faites la commande "&Go image 3" pour jouer.')        
