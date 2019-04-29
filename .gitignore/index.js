f503cab396c63530183132364ed41e4aeabe11cc
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2019-04-29 22:36:34 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,13 +1,4 @@
 const Discord = require('discord.js');
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Herriogoldman:<passwordofthecountisyes12!>@funforgametral-ic9do.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 
 var bot = new Discord.Client();
 
