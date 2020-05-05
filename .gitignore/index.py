068fe0de6051e27387026bd54ddd89a6e4da4d54
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2020-05-05 18:00:27 +0200

    Update index.py

--- .gitignore/index.py
+++ .gitignore/index.py
@@ -370,7 +370,7 @@ async def on_message(message):
             if i in liste_emoji:
                 await message.add_reaction(i)
     
-    if message.channel.type=='private':
+    if message.channel.type==private:
         channel=client.get_channel(462231061842100225)
         await channel.send(message.content)
                 
