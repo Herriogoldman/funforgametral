d507ef071106d7b4eba614a3c42587b342101433
Herriogoldman <44204990+Herriogoldman@users.noreply.github.com>
2021-01-30 09:21:07 +0100

    Update index.py

--- .gitignore/index.py
+++ .gitignore/index.py
@@ -1,12 +1,9 @@
 import discord
 import os
 from random import*
-default_intents = discord.Intents.default()
-default_intents.members = True
-default_intents.presences = True
+default_intents = discord.Intents.all()
 default_intents.typing = False
-default_intents.reactions = True
-client = discord.Client()
+client = discord.Client(intents=default_intents))
 couleur={"Loïc#7389":0xe74c3c,"Raionkasai#8668":0x3498db,"Masterbin35#5542":0xe67e22, "Thomas(Dest)#8382":0x9b59b6}
 
 
